eee7ec4f4d4ab2d4eb5bb3d9977ed60b455381dd
tiephust <145523459+tiephust@users.noreply.github.com>
2025-03-20 03:48:13 +0700

    add ValidPassword annotation for password validation

---
+++ src/main/java/management/residentmanagement/validation/ValidPassword.java
@@ -0,0 +1,11 @@
+package management.residentmanagement.validation;
+
+import jakarta.validation.Payload;
+
+public @interface ValidPassword {
+    String message() default "Password must include at least one uppercase letter and one special character"; // Password phải bao gồm ít nhất một chữ in hoa và một ký tự đặc biệt
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
