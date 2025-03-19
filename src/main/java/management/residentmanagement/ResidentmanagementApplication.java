7a8c9799ea15679b0b03a59831229ec5d153a2d9
tiephust <145523459+tiephust@users.noreply.github.com>
2025-03-20 03:51:00 +0700

    add UserService import and prepare for future login checks

--- src/main/java/management/residentmanagement/ResidentmanagementApplication.java
+++ src/main/java/management/residentmanagement/ResidentmanagementApplication.java
@@ -1,13 +1,18 @@
 package management.residentmanagement;
 
+import management.residentmanagement.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ResidentmanagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ResidentmanagementApplication.class, args);
+//        ApplicationContext context = SpringApplication.run(ResidentmanagementApplication.class, args);
+//        UserService userService = context.getBean(UserService.class);
+//        userService.checkLogin("123456789", "admin123");
     }
 
 }
