c9811afe86607003af98bb03ea82acdad990cab1
tiephust <145523459+tiephust@users.noreply.github.com>
2025-03-20 03:52:03 +0700

    add global exception handler and custom exceptions for user management

---
+++ src/main/java/management/residentmanagement/controller/RegisterController.java
@@ -0,0 +1,9 @@
+package management.residentmanagement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class RegisterController {
+}
